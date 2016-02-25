83d081c18e0d42efdd39892300a46b96216b1c4c
Simon Egli <simon.egli@bbv.ch>
2016-02-25 16:15:58 +0100

    Working paginator
    Signed-off-by: Simon Egli <simon.egli@bbv.ch>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,29 @@
 
 Turbolinks.enableProgressBar();
 $(document).ready( function() {
+    var QueryString = function () {
+        // This function is anonymous, is executed immediately and 
+        // the return value is assigned to QueryString!
+        var query_string = {};
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            // If first entry with this name
+            if (typeof query_string[pair[0]] === "undefined") {
+                query_string[pair[0]] = decodeURIComponent(pair[1]);
+                // If second entry with this name
+            } else if (typeof query_string[pair[0]] === "string") {
+                var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+                query_string[pair[0]] = arr;
+                // If third or later entry with this name
+            } else {
+                query_string[pair[0]].push(decodeURIComponent(pair[1]));
+            }
+        } 
+        return query_string;
+    }();
+
  $(".table-clickeable").on("click", "tr", function() {
             var url = $( this ).data("url");
             if((typeof url !== 'undefined') && (url != "")) {
@@ -27,10 +50,6 @@ $(document).ready( function() {
 
  $(".load-more").on("click",  function() {
      $( this )[0].innerHTML = "Loading..."
-            var url = $( this ).data("url");
-            document.location.href = document.location.href+"?page=2"
-            //if((typeof url !== 'undefined') && (url != "")) {
-            //document.location.href = url;
-            //}
+     document.location.href = window.location.href.split('?')[0]+"?page="+(parseInt(QueryString.page)+1)
              });
 });
